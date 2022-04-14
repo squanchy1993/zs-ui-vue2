443dd88477ea1c04591dc6c4f2caf7504ebe7306
朱志森 <826482354@qq.com>
2022-04-14 14:14:00 +0800

    change dependencies

---
+++ packages/shared/lib/src/tool/index.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: your name
+ * @Date: 2021-03-16 08:58:22
+ * @LastEditTime: 2022-04-14 14:08:24
+ * @LastEditors: Please set LastEditors
+ * @Description: 模块化导出
+ * @FilePath: /robot-web/src/utils/public/index.js
+ */
+
+export * from './lib/common'
+export * from './lib/storage'
+
