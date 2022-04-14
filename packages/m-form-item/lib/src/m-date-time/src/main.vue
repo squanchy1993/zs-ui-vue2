228b5fa729e0d3f789b285ef2a16e2cb0fab3e47
朱志森 <826482354@qq.com>
2022-04-14 13:23:21 +0800

    update: form-item

---
+++ packages/m-form-item/lib/src/m-date-time/src/main.vue
@@ -0,0 +1,111 @@
+<!--
+    // 时间段配置
+    new MFormItemModel({
+        title: "日期时间选择器",
+        keywords: "startTime-endTime",
+        el_type: "MDateTime",
+        options: {
+            dateTimeType: "daterange",
+            valueFormat: "yyyy-MM-dd HH:mm:ss",
+            pickerOptions: {
+                disabledDate(time) {
+                    return time.getTime() > Date.now() - 8.64e7;
+                },
+            }
+        }
+    }),
+
+    // 单个时间配置
+    new MFormItemModel({
+        title: "开始时间",
+        keywords: "scheduledTime",
+        el_type: "MDateTime",
+        options: {
+            dateTimeType: "datetime",
+            valueFormat: "yyyy-MM-dd HH:mm:ss",
+            pickerOptions: {
+                disabledDate(time) {
+                return time.getTime() < Date.now();
+                }
+            }
+        }
+    })
+
+    // 其他
+    time.getTime() < Date.now() - 8.64e7;//当天之后的时间可选
+    time.getTime() > Date.now() - 8.64e7;//当天之前的时间可选---不包括当天
+    time.getTime() > Date.now() ;//当天之前的时间可选---包括当天
+-->
+
+<template>
+  <el-form-item
+    class="form-item"
+    v-bind="formItem"
+  >
+    <el-date-picker
+      v-model="componentValue"
+      class="item_input"
+      v-bind="formItem.options"
+      align="right"
+      size="mini"
+      unlink-panels
+      range-separator="至"
+      start-placeholder="开始日期"
+      end-placeholder="结束日期"
+      @blur="componentValueChange(componentValue)"
+    />
+  </el-form-item>
+</template>
+
+<script>
+export default {
+  name: 'MDateTime',
+  model: {
+    prop: 'value',
+    event: 'valueChange'
+  },
+  props: {
+    formItem: {
+      type: Object,
+      default: () => {
+        return {}
+      }
+    },
+    value: {
+      type: [String, Number, Array],
+      default: null
+    }
+  },
+  data() {
+    return {
+      componentValue: ''
+    }
+  },
+  computed: {
+    inputValueChange() {
+      // 监听输入value的变化
+      return this.value
+    }
+  },
+  watch: {
+    inputValueChange: {
+      immediate: true,
+      handler: function(value) {
+        // 延迟到组件创建完毕后再进行
+        this.$nextTick(() => {
+          if (value != null) {
+            this.componentValue = this._.cloneDeep(value)
+          }
+        })
+      }
+    }
+  },
+  methods: {
+    // 通知父元素已经变化
+    componentValueChange(value) {
+      this.$emit('valueChange', value)
+    }
+  }
+}
+</script>
+
---
+++ packages/m-form-item/lib/src/m-img-upload/src/ImgUploadItem.vue
@@ -0,0 +1,166 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-07-23 13:29:12
+ * @LastEditTime: 2022-03-04 20:07:30
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /micro-rail/src/views/system/robot/components/forms/robot-base-info/w-img-upload/index.vue
+-->
+<!-- 单文件上传组件 -->
+<template>
+  <div class="upload-item">
+    <img v-show="!disabled" class="item-delete" :src="deleteImg" mode="aspectFit" alt="图片" @click="deleteFun">
+    <div class="item-background">
+      <el-image :z-index="9999" class="item-preview" :src="imgData.localPath" mode="aspectFit" alt="图片" :preview-src-list="[imgData.localPath]" />
+    </div>
+    <div v-if="imgData.upload_state === 'uploading'" class="item-process">
+      <div class="process-width" :style="{ 'width': imgData.upload_progress + '%',transition:'1s ease-in-out'}" />
+    </div>
+    <div v-if="imgData.upload_state === 'fail'" class="item-fail">
+      <span>{{ imgData.tips }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WImgUploadItem',
+  model: {
+    prop: 'value',
+    event: 'valueChange'
+  },
+  props: {
+    value: {
+      type: Object,
+      default: () => {
+        return {}
+      }
+    },
+    api: {
+      type: Function,
+      default: () => {
+        return new Function()
+      }
+    },
+
+    // 是否禁用
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      imgData: null,
+      deleteImg: require('./delete.png'),
+      upload_progress: 0
+    }
+  },
+  computed: {
+    computedValue() {
+      return this.value
+    }
+  },
+  watch: {
+    computedValue: {
+      immediate: true,
+      handler: function(value) {
+        this.imgData = { ...value }
+        const { upload_state } = value
+        if (upload_state === 'uploading') {
+          this.upload()
+        }
+      }
+    }
+  },
+  methods: {
+    // 上传图片
+    upload() {
+      this.imgData.upload_progress = 100 * Math.random()
+      const form = new FormData()
+      const { file } = this.imgData
+      form.append('file', file)
+      this.api(form).then(res => {
+        if (res.code) {
+          this.imgData.upload_progress = 100
+          this.imgData.upload_state = 'success'
+          this.imgData.tips = '上传成功'
+          this.imgData.src = res.data
+          this.$emit('valueChange', this.imgData)
+          this.$emit('uploaded')
+        } else {
+          throw new Error(res.msg)
+        }
+      }).catch(() => {
+        this.imgData.upload_progress = 0
+        this.imgData.upload_state = 'fail'
+        this.imgData.tips = '上传失败！'
+      })
+    },
+
+    // 删除
+    deleteFun() {
+      this.$emit('delete', this.imgData.id)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped >
+.upload-item {
+  overflow: hidden;
+  position: relative;
+  .item-delete{
+    z-index: 12;
+    position: absolute;
+    right: 0;
+    top: 0;
+    width: 25%;
+    height: 25%
+  }
+  .item-background {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 10;
+    .item-preview {
+      width: 100%;
+    }
+  };
+  .item-process{
+      position: absolute;
+      left: 5%;
+      bottom: 10%;
+      width: 90%;
+      height: 5%;
+      background-color: #fefefe;
+      border-radius: 10px;
+      overflow: hidden;
+      z-index: 11;
+      .process-width{
+          height: 100%;
+          width: 0%;
+          background-color:#409eff;
+      }
+  }
+
+  .item-fail{
+    position: absolute;
+    z-index: 11;
+    width: 100%;
+    height: 100%;
+    background:rgba($color: #000000, $alpha: 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    span {
+      color: #fff
+    }
+  }
+}
+</style>
