0281ad4e90ddfd77f83fb8e0ceb298e3b12241dd
朱志森 <826482354@qq.com>
2022-04-15 19:51:51 +0800

    fix eslint

--- packages/m-form/lib/m-page-list/index.js
+++ packages/m-form/lib/m-page-list/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-04-11 13:53:33
- * @LastEditTime: 2022-04-14 11:13:10
+ * @LastEditTime: 2022-04-15 19:38:35
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /zs-ui-vue/packages/m-form-dialog/index.js
@@ -14,4 +14,4 @@ MPageList.install = function(Vue) {
   Vue.component(MPageList.name, MPageList);
 };
 
-export{ MPageList, MPageListModel };
+export { MPageList, MPageListModel };
--- packages/shared/lib/src/tool/index.js
+++ packages/shared/lib/src/tool/index.js
@@ -1,12 +1,15 @@
 /*
  * @Author: your name
  * @Date: 2021-03-16 08:58:22
- * @LastEditTime: 2022-04-14 14:08:24
+ * @LastEditTime: 2022-04-15 19:18:37
  * @LastEditors: Please set LastEditors
  * @Description: 模块化导出
  * @FilePath: /robot-web/src/utils/public/index.js
  */
+import CommonUtils from './lib/common'
+import StorageUtils from './lib/storage'
 
-export * from './lib/common'
-export * from './lib/storage'
-
+export {
+  CommonUtils,
+  StorageUtils
+}
