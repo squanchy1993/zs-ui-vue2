4ac557f286d5e76836611ae310073b272654df2e
squanchy1993 <squanchy@yeah.net>
2022-04-16 18:08:05 +0800

    fix: add dialog

---
+++ packages/m-form/lib/m-form-dialog/src/mian.vue
@@ -0,0 +1,176 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-06-15 11:12:27
+ * @LastEditTime: 2022-04-16 18:06:01
+ * @LastEditors: Please set LastEditors
+ * @Description: type 的设置 可以写在一个组件里面，可以单独拉出来，看你这个组件复杂不，复杂的话一个模块一个type
+ * @FilePath: /vue-admin-template/src/views/sidebars/system/permission/index.vue
+-->
+<template>
+  <div class="book-dialog">
+    <el-dialog
+      class="list-dialog"
+      :before-close="
+        () => {
+          reject();
+          close();
+        }
+      "
+      :visible.sync="isShow"
+      v-bind="dialogOption.dialogAtter"
+      custom-class="m-dialog"
+      center
+    >
+      <div class="list-dialog__container">
+        <el-form v-if="isShow" :ref="dialogOption.ref" class="m-form" :model="formData" size="mini">
+          <m-form-item
+            :form-items.sync="formItems"
+            :form-data.sync="formData"
+            :form-item-class="'dialogFormItem'"
+          />
+        </el-form>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import { MFormItem } from '../../m-form-item/index'
+import { DialogOptionModel } from './model'
+
+export default {
+  name: 'FormDialog',
+  components: {
+    MFormItem
+  },
+  props: {
+    dialogOption: {
+      type: Object,
+      default: () => {
+        return new DialogOptionModel({})
+      }
+    }
+  },
+  data() {
+    return {
+      formItems: [],
+      formData: {},
+      formOriginData: null,
+      formStatus: 'create', // create、 edit
+      resolve: null,
+      reject: null,
+      isShow: false
+    }
+  },
+  methods: {
+    // 打开对话框
+    async openDialog({ type = 'create', dialogData = {}}) {
+      const promise = new Promise((resolve, reject) => {
+        // eslint-disable-next-line prefer-const
+        let { originFormItems, originFormData } = this.dialogOption.generateFormItems({ that: this, openDialogData: dialogData })
+
+        for (var item of originFormItems) {
+          if (item.prop) {
+            if (type === 'auth' || type === 'check') {
+              item.disabled = true
+            }
+            // 当dialogData的模型和formitems的一致时可以取巧，不一致的时候还是要一个个设置
+            const dialogDataValue = this._.get(dialogData, item.prop)
+            let setValue
+            if (!this._.isNumber(dialogDataValue)) {
+              setValue = !this._.isEmpty(dialogDataValue) ? dialogDataValue : item.defaultValue
+            } else {
+              setValue = !this._.isNil(dialogDataValue) ? dialogDataValue : item.defaultValue
+            }
+            console.log('this._.isEmpty(setValue)', setValue)
+
+            this._.set(originFormData, item.prop, setValue)
+          }
+        }
+        this.formItems = originFormItems
+        this.formData = originFormData
+        this.formOriginData = { ...dialogData }
+        this.formStatus = type
+        this.resolve = resolve
+        this.reject = reject
+        this.isShow = true
+      })
+      return promise
+    },
+
+    // 点击
+    close() {
+      this.$nextTick(() => {
+        this.resetDialog()
+      })
+    },
+
+    // 重置
+    resetDialog() {
+      this.formItems = []
+      this.formData = {}
+      this.formOriginData = {}
+      this.formStatus = 'create'
+      this.resolve = null
+      this.reject = null
+      this.isShow = false
+    },
+
+    // 提交
+    submit(extraData) {
+      this.$refs[this.dialogOption.ref]
+        .validate()
+        .then(async(res) => {
+          let submitInterceptor
+          switch (this.formStatus) {
+            case 'create':
+              submitInterceptor = this.dialogOption.createInterceptor
+              break
+            case 'edit':
+              submitInterceptor = this.dialogOption.editInterceptor
+              break
+            case 'auth':
+              submitInterceptor = this.dialogOption.authInterceptor
+              break
+          }
+          // eslint-disable-next-line prefer-const
+          let { params, api, message, resolveData = {}} = submitInterceptor(this.formOriginData, this.formData, extraData)
+          if (api) {
+            resolveData = await api(params)
+            message && this.$message.success(message)
+          }
+          this.resolve(resolveData)
+          this.dialogOption.submitSuccess()
+          this.close()
+        })
+        .catch((err) => {
+          console.log('表单检验失败', err)
+          this.$message.error(`提交失败: ${err.message}`)
+        })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.list-dialog {
+  .list-dialog__container {
+    max-height: 60vh;
+    overflow: scroll;
+    .m-form {
+      ::v-deep {
+        .title {
+          height: 35px;
+          background-color: #ecf8ff;
+          line-height: 35px;
+          background-color: #ecf8ff;
+          border-radius: 4px;
+          border-left: 5px solid #50bfff;
+          margin: 20px 0;
+          padding-left: 10px;
+        }
+      }
+    }
+  }
+}
+</style>
---
+++ packages/m-form/lib/m-form-dialog/src/model.js
@@ -0,0 +1,46 @@
+/*
+ * @Author: your name
+ * @Date: 2022-04-16 18:01:44
+ * @LastEditTime: 2022-04-16 18:03:11
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /zs-ui-vue/packages/m-form/lib/m-form-dialog/src/model.js
+ */
+const _ = require('lodash')
+
+export class DialogOptionModel {
+  constructor({
+    dialogAtter = {},
+    generateFormItems = () => {
+      return { originFormItems: [], originFormData: {}}
+    },
+    createInterceptor = (formOriginData, formData, extraData) => { },
+    editInterceptor = (formOriginData, formData, extraData) => {},
+    authInterceptor = (formOriginData, formData, extraData) => {},
+    submitSuccess = () => {}
+  }) {
+    // 弹框ref
+    this.ref = _.uniqueId('form_')
+
+    // 生成表单
+    this.generateFormItems = generateFormItems
+
+    // 生成api
+    this.createInterceptor = createInterceptor
+    this.editInterceptor = editInterceptor
+    this.authInterceptor = authInterceptor
+
+    // 提交结束
+    this.submitSuccess = submitSuccess
+
+    // 弹框设置
+    let defaultDialogAtter = {
+      title: '',
+      'destroy-on-close': true,
+      'modal-append-to-body': false,
+      'close-on-click-modal': false
+    }
+    this.dialogAtter = _.defaults({}, dialogAtter, defaultDialogAtter)
+    console.log(this.dialogAtter)
+  }
+}
