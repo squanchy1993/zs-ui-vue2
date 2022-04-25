292dddd53805694279bf2be3ae464e33c357e87b
squanchy1993 <squanchy@yeah.net>
2022-04-25 16:31:28 +0800

    fix: 打包配置

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: your name
+ * @Date: 2022-04-25 10:46:29
+ * @LastEditTime: 2022-04-25 16:27:36
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /zs-ui-vue/babel.config.js
+ */
+module.exports = {
+  'presets': ['@babel/preset-env'],
+  'plugins': ['jsx-vue-functional', 'transform-vue-jsx']
+  //  使用jsx插件
+}
