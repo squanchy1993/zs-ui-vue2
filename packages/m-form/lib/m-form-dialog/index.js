25aaa25e68b7871050819a5588fb0a08c6f35b06
squanchy1993 <squanchy@yeah.net>
2022-04-27 11:14:58 +0800

    fix: 修改组件名字

--- packages/m-form/lib/m-form-dialog/index.js
+++ packages/m-form/lib/m-form-dialog/index.js
@@ -1,17 +1,17 @@
 /*
  * @Author: your name
  * @Date: 2022-04-16 18:00:39
- * @LastEditTime: 2022-04-17 14:43:55
+ * @LastEditTime: 2022-04-27 11:13:42
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /zs-ui-vue/packages/m-form/lib/m-form-dialog/index.js
  */
-import FormDialog from './src/mian';
+import MFormDialog from './src/mian';
 import { DialogOptionModel } from './src/model'
 
 /* istanbul ignore next */
-FormDialog.install = function(Vue) {
-  Vue.component(FormDialog.name, FormDialog);
+MFormDialog.install = function(Vue) {
+  Vue.component(MFormDialog.name, MFormDialog);
 };
 
-export { FormDialog, DialogOptionModel };
+export { MFormDialog, DialogOptionModel };
