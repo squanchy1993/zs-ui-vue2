dea8c527a2cd51430bf2836eda0ede0fc447db2f
squanchy1993 <squanchy@yeah.net>
2022-04-27 11:30:19 +0800

    fix: useless

--- packages/m-form/lib/m-form-dialog/index.js
+++ packages/m-form/lib/m-form-dialog/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-04-16 18:00:39
- * @LastEditTime: 2022-04-27 11:13:42
+ * @LastEditTime: 2022-04-27 11:29:58
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /zs-ui-vue/packages/m-form/lib/m-form-dialog/index.js
@@ -9,7 +9,6 @@
 import MFormDialog from './src/mian';
 import { DialogOptionModel } from './src/model'
 
-/* istanbul ignore next */
 MFormDialog.install = function(Vue) {
   Vue.component(MFormDialog.name, MFormDialog);
 };
