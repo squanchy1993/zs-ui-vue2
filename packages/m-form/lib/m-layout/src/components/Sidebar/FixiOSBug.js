f0d0fd2c9e0da929cae31baa16872db925ddc9c4
squanchy1993 <squanchy@yeah.net>
2022-06-17 15:09:06 +0800

    feat: add m-form

---
+++ packages/m-form/lib/m-layout/src/components/Sidebar/FixiOSBug.js
@@ -0,0 +1,30 @@
+export default {
+  inject: ['layoutState', 'operator'],
+  computed: {
+    settingState() {
+      return this.layoutState()
+    },
+    device() {
+      return this.settingState.device
+    }
+  },
+  mounted() {
+    // In order to fix the click on menu on the ios device will trigger the mouseleave bug
+    // https://github.com/PanJiaChen/vue-element-admin/issues/1135
+    this.fixBugIniOS()
+  },
+  methods: {
+    fixBugIniOS() {
+      const $subMenu = this.$refs.subMenu
+      if ($subMenu) {
+        const handleMouseleave = $subMenu.handleMouseleave
+        $subMenu.handleMouseleave = (e) => {
+          if (this.device === 'mobile') {
+            return
+          }
+          handleMouseleave(e)
+        }
+      }
+    }
+  }
+}
