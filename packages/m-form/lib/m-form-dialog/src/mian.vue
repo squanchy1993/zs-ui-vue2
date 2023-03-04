783bfcaf8d4805782255eca8cf6247bd376bfcd3
squanchy <squanchy@yeah.net>
2023-03-04 19:04:10 +0800

    fix: change height

--- packages/m-form/lib/m-form-dialog/src/mian.vue
+++ packages/m-form/lib/m-form-dialog/src/mian.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2021-06-15 11:12:27
- * @LastEditTime: 2023-03-04 19:00:31
+ * @LastEditTime: 2023-03-04 19:03:23
  * @LastEditors: squanchy squanchy@yeah.net
  * @Description: type 的设置 可以写在一个组件里面，可以单独拉出来，看你这个组件复杂不，复杂的话一个模块一个type
  * @FilePath: /zs-ui-vue/packages/m-form/lib/m-form-dialog/src/mian.vue
@@ -191,10 +191,10 @@ export default {
       display: flex;
       flex-direction: column;
       .el-dialog__header {
-        height: 65px;
+        height: 60px;
       }
       .el-dialog__body {
-        max-height: calc(80vh - 65px);
+        max-height: calc(80vh - 60px);
         overflow: scroll;
         box-sizing: border-box;
         &::-webkit-scrollbar {
