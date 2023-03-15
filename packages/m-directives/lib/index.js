efaf3f474cb69ba668d79696341c372fac0e983d
squanchy1993 <squanchy@yeah.net>
2023-03-15 16:15:13 +0800

    fix: update markdown and import export

--- packages/m-directives/lib/index.js
+++ packages/m-directives/lib/index.js
@@ -1,2 +1,6 @@
 'use strict';
-export { default as LongPress } from './src/longPress'
+// export { default as LongPress } from './src/longPress'
+
+import LongPress from './src/longPress'
+export { LongPress }
+export default { LongPress }
