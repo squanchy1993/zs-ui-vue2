932e3291e81620c6f348bc78eff6b3379f2fa4de
squanchy1993 <squanchy@yeah.net>
2023-03-17 10:35:04 +0800

    adfa

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,9 +1,13 @@
-/*
- * @Author: your name
- * @Date: 2022-04-17 15:51:56
- * @LastEditTime: 2022-04-17 15:51:56
- * @LastEditors: Please set LastEditors
- * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- * @FilePath: /zs-ui-vue2/commitlint.config.js
- */
-module.exports = { extends: ['@commitlint/config-conventional'] }
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        //0为disable，1为warning，2为error
+        //always|never
+        //第三位该rule的值
+        'type-enum': [2, 'always', [
+            'feature', 'update', 'fixbug', 'refactor', 'optimize', 'style', 'docs', 'chore'
+        ]],
+        'subject-full-stop': [0, 'never'],
+        'subject-case': [0, 'never']
+    }
+}
