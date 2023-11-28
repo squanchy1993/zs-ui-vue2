c79d89894fd6ae076dcfd18073a24257e893808c
squanchy1993 <squanchy@yeah.net>
2023-11-28 21:55:14 +0800

    fix: optimize code

--- packages/m-components/lib/components/m-design/src/design-table-dragge-out.vue
+++ packages/m-components/lib/components/m-design/src/design-table-dragge-out.vue
@@ -1,22 +1,23 @@
 <!--
  * @Date: 2023-11-15 21:03:14
  * @LastEditors: squanchy1993 squanchy@yeah.net
- * @LastEditTime: 2023-11-20 11:37:18
- * @FilePath: \m-components\lib\components\m-design\src\design-table-dragge-out.vue
+ * @LastEditTime: 2023-11-28 21:45:06
+ * @FilePath: /m-components/lib/components/m-design/src/design-table-dragge-out.vue
 -->
 <template>
-  <div ref="draggeRef" class="design-table-dragge-out">
-    <div
-      v-for="({ props: { prop, label }, elemOptions }, index) in fields"
-      :key="index"
-      class="dragge-out__item el-table__cell"
-    >
-      <p class="title">{{ label }}</p>
-      <MDynamicElem :config="elemOptions" :prop-key="prop" :data="{}" />
+  <el-form size="mini" label-width="100px">
+    <div ref="draggeRef" class="design-table-dragge-out">
+      <el-form-item
+        v-for="({ props: { prop, label }, elemOptions }, index) in fields"
+        :key="index"
+        class="dragge-out__item el-table__cell"
+        :label="label"
+      >
+        <MDynamicElem :config="elemOptions" :prop-key="prop" :data="{}" />
+      </el-form-item>
     </div>
-  </div>
+  </el-form>
 </template>
-
 <script>
 import Sortable from 'sortablejs';
 import { MDynamicElem } from '../../m-dynamic-elem';
@@ -76,20 +77,16 @@ export default {
 
 <style scoped lang="scss">
 .design-table-dragge-out {
-  width: 100%;
+  width: 200px;
   display: grid;
   flex-direction: column;
   grid-template-columns: 1;
   grid-row-gap: 10px;
   .dragge-out__item {
-    width: 100%;
+    width: 200px;
     background-color: #e0d5d5;
     border-radius: 5px;
     padding: 5px;
-    .title {
-      width: 100%;
-      line-height: 28px;
-    }
   }
 }
 </style>
